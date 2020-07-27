2a0eaebcc0da0b57241947a139ea2889d7099a7e
Dru Serkes <DruSerkes@gmail.com>
2020-07-27 15:20:09 -0400

    add, style reset button

--- src/EightBall.js
+++ src/EightBall.js
@@ -11,18 +11,25 @@ const EightBall = ({ answers }) => {
 		setMessage(answer.msg);
 		setColor(answer.color);
 	};
+	const reset = () => {
+		setMessage('Think of a question');
+		setColor('black');
+	};
 
 	const [ message, setMessage ] = useState('Think of a question');
 	const [ color, setColor ] = useState('black');
 
 	return (
-        <>
-        <h1>Magic 8-Ball</h1>
-		<div className="EightBall" style={{ backgroundColor: color }} onClick={getAnswer}>
-			<p className="EightBallText">{message}</p>
+		<div>
+			<h1>Magic 8-Ball</h1>
+			<div className="EightBall" style={{ backgroundColor: color }} onClick={getAnswer}>
+				<p className="EightBallText">{message}</p>
+			</div>
+			<p>Click the ball to recieve your answer</p>
+			<button className="EightBallReset" onClick={reset}>
+				Reset
+			</button>
 		</div>
-        <p>Click the ball to recieve your answer</p>
-        </>
 	);
 };
 
