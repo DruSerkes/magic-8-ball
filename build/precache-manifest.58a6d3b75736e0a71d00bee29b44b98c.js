159189ecb867c15ccdbef5993247e72a024b2d6f
Dru Serkes <DruSerkes@gmail.com>
2020-07-27 15:59:47 -0400

    run build

---
+++ build/precache-manifest.58a6d3b75736e0a71d00bee29b44b98c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f803b18cef240c5ea912eb670c23c417",
+    "url": "/magic-8-ball/index.html"
+  },
+  {
+    "revision": "cd59c43c9e5a4cc72e19",
+    "url": "/magic-8-ball/static/css/main.e51a8d30.chunk.css"
+  },
+  {
+    "revision": "1c04bf496fbcbdb38dc8",
+    "url": "/magic-8-ball/static/js/2.dc183ee9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/magic-8-ball/static/js/2.dc183ee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd59c43c9e5a4cc72e19",
+    "url": "/magic-8-ball/static/js/main.d4efb59b.chunk.js"
+  },
+  {
+    "revision": "c8bf408dd5b7051d3af9",
+    "url": "/magic-8-ball/static/js/runtime-main.51951ff0.js"
+  }
+]);
