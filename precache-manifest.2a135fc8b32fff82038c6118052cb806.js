6673f6e46249dcdc23a3f4adc804c09f7374b99f
Dru Serkes <DruSerkes@gmail.com>
2025-03-16 16:31:00 -0400

    Updates

---
+++ precache-manifest.2a135fc8b32fff82038c6118052cb806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f07ab8a01e1c5c3d53af4e26dc06ba83",
+    "url": "/magic-8-ball/index.html"
+  },
+  {
+    "revision": "91ad18c0bc5a85a86abe",
+    "url": "/magic-8-ball/static/css/main.df33a44a.chunk.css"
+  },
+  {
+    "revision": "1c04bf496fbcbdb38dc8",
+    "url": "/magic-8-ball/static/js/2.dc183ee9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/magic-8-ball/static/js/2.dc183ee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91ad18c0bc5a85a86abe",
+    "url": "/magic-8-ball/static/js/main.b6cd7cdc.chunk.js"
+  },
+  {
+    "revision": "c8bf408dd5b7051d3af9",
+    "url": "/magic-8-ball/static/js/runtime-main.51951ff0.js"
+  }
+]);
